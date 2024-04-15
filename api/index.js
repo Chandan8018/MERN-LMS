e1ecd9083b3be13007ebcd8dc74c90e492848054
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-04-15 10:26:32 +0530

    lms

---
+++ api/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import userRouters from "./routes/user.route.js";
+import authRouters from "./routes/auth.route.js";
+import postRouters from "./routes/post.route.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb is connected..");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server running on port number 3000");
+});
+
+app.use("/api/user", userRouters);
+app.use("/api/auth", authRouters);
+app.use("/api/post", postRouters);
+
+//Error-Handling Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
---
+++ api/models/post.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://img.freepik.com/premium-vector/product-release-business-concept_198530-878.jpg",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    qty: {
+      type: Number,
+      default: 1,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
